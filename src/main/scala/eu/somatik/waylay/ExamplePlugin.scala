4b1f98f8c14703dd92e87e4b819d51e66f675b95
Francis De Brabandere <francisdb@gmail.com>
2021-07-09 11:10:32 +0200

    scalafmt tweak

--- src/main/scala/eu/somatik/waylay/ExamplePlugin.scala
+++ src/main/scala/eu/somatik/waylay/ExamplePlugin.scala
@@ -6,9 +6,7 @@ import scala.scalajs.js.JSON
 
 object ExamplePlugin extends Plugin {
 
-  override def run(
-      options: js.Object
-  ): Future[(js.Any, js.Object)] = {
+  override def run(options: js.Object): Future[(js.Any, js.Object)] = {
     println("Hello from scala.js!")
     println(s"Got options: ${JSON.stringify(options, space = 2)}")
     val value = js.Dynamic.literal(
