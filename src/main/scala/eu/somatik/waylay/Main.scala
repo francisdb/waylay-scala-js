921636bad818ee805a63c4c9041330567df3eced
Francis De Brabandere <francisdb@gmail.com>
2021-07-09 10:28:48 +0200

    remove test code in main as we now have proper tests

--- src/main/scala/eu/somatik/waylay/Main.scala
+++ src/main/scala/eu/somatik/waylay/Main.scala
@@ -4,30 +4,12 @@ import eu.somatik.waylay.ExamplePlugin.run
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
-import scala.scalajs.js.JSON
 import scala.scalajs.js.annotation.JSGlobalScope
 
 object Main {
-  private val options =
-    if (js.typeOf(Globals.options) == "undefined")
-      js.Dynamic.literal(
-        "foo" -> 42,
-        "tic" -> "toc"
-      )
-    else Globals.options
-
-  // debug replacement for send()
-  private val send: (js.Any, js.Object) => Unit =
-    if (js.typeOf(js.Dynamic.global.send) == "undefined") testSend
-    else Globals.send
-
   def main(args: Array[String]): Unit = {
-    run(options).map { case (err, result) => send(err, result) }
+    run(Globals.options).map { case (err, result) => Globals.send(err, result) }
   }
-
-  private def testSend(err: js.Any, result: js.Object): Unit =
-    println(s"Debug send($err,${JSON.stringify(result, space = 2)})")
-
 }
 
 @js.native
